8a22218744a23705dd69fdd0cbed990c4899a00c
romain97 <r.melun@live.fr>
2016-04-06 14:18:04 +0200

    creation-footer

---
+++ footer.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<script type="text/javascript" src="./js/bootstrap.js"></script>
+		<script type="text/javascript" src="./js/script.js"></script>
+		<link type="text/css" rel="stylesheet" href="./css/bootstrap.css">
+		<link type="text/css" rel="stylesheet" href="./css/style.css">
+	</head>
+	<body>
+		<div class="col-sm-12 col-md-12 col-lg-12 footer-main">
+			<div class="col-sm-4 col-md-4 col-lg-4 footer-section left">
+				<p>41 Rue Robert Poisson </p>
+				<p>28130 SAINT PIAT </p>
+				<p>PARIS : Place d'Italie, Nationale</p>
+			</div>	
+			<div class="col-sm-4 col-md-4 col-lg-4 footer-section left">
+				<p>06 87 68 13 99  </p>
+				<p>annie.bearez@wanadoo.fr</p>
+			</div>
+			<div class="col-sm-4 col-md-4 col-lg-4 footer-section right">
+				<p>Plan du site</p>
+                <p> Mentions legales &copy;</p> 
+				<p>SIRET : 385 293 758 00038</p>
+			</div>
+		</div>
+	</body>
+</html>
