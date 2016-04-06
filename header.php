98ad4eb800295d1df14c39660568768b008c089d
FredGOVIGNON <fgovignon@gmail.com>
2016-04-06 13:49:18 +0200

    header css images

---
+++ header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>ABCoaching</title>
+        <script tyep="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script type="text/javascript" src="./js/bootstrap.min.js"></script>
+        <link type="text/css" rel="stylesheet" href="./css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="./css/main.css"/>
+        <link rel="stylesheet" type="text/css" href="./css/style_fred.css"/>
+    </head>
+    <body>
+    	<div class="container">
+            <div class="row">
+                <header class="col-md-12">
+    				<nav class="navbar navbar-default navbar-fixed-top">
+    			  		<div class="container-fluid">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <a class="navbar-brand" href="index_fred.php">
+                                    <img alt="nautile" src="./images/petit_nautile_bleu.png" class="nautile">
+                                </a>
+                            </div>
+                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                              <ul class="nav navbar-nav">
+                                <li class="active"><a href="index.php">Accueil</a></li>
+                                <li><a href="#">Qui suis-je ?</a></li>
+                                <li><a href="#">Mes prestations</a></li>
+                                <li><a href="#">Références</a></li>
+                                <li><a href="#">Liens</a></li>
+                                <li><a href="#">Témoignages</a></li>
+                                <li><a href="#">Blog</a></li>
+                              </ul>
+                              <ul class="nav navbar-nav navbar-right">
+                                <li><a href="contact.php">Contact</a></li>
+                                <li><a class="linked_in" href="#"><img alt="Brand" src="./images/Linked_In.png"></a></li>
+                              </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </header>
+            </div>
+        </div>
+    </body>
+</html>
