2e264d038a3cbf348303992ebda3d68daa8e82c0
etidimjav <thomas.javanaud@gmail.com>
2016-04-09 22:34:11 +0200

    MAJ index et header

--- index.php
+++ index.php
@@ -13,9 +13,15 @@
 
     <script>
       $(document).ready(function(){
-        $("#Apprendre").animate({left: '100px'}); 
-        $("#Accompagner").animate({left: '250px'});
-        $("#Developper").animate({left: '450px'});   
+        if($(window).width() > 700){
+          $("#Apprendre").animate({left: '100px'}); 
+          $("#Accompagner").animate({left: '250px'});
+          $("#Developper").animate({left: '450px'}); 
+        } else {
+          $("#Apprendre").animate({left: '25px'}); 
+          $("#Accompagner").animate({left: '75px'});
+          $("#Developper").animate({left: '125px'}); 
+        }
       });
     </script>
 
@@ -27,30 +33,32 @@
       include("header.php")
     ?>
      
-    <div class="index_section1_texte">
-      <div id="Apprendre">
-      <a class="non_soulignement" href="./formations.php"> Apprendre</a></div>
-      <div id="Accompagner">
-      <a class="non_soulignement" href="./coaching.php">Accompagner</a></div>
-      <div id="Developper">
-      <a class="non_soulignement" href="./ateliers.php"> Développer</a></div>
-     
+    <div class="row index_section1">
+      <div class="index_section1_texte">    
+        <div id="Apprendre">
+        <a class="non_soulignement" href="./formations.php"> Apprendre</a>
+        </div>
+        <div id="Accompagner">
+        <a class="non_soulignement" href="./coaching.php">Accompagner</a>
+        </div>
+        <div id="Developper">
+        <a class="non_soulignement" href="./ateliers.php"> Développer</a>
+        </div>
+      </div>
     </div>
-
-    <div class="index_section1_photo"></div>
 
 
     <div class="index_section2_presentation">
-        <div class="row">
-          <div class="col-lg-offset-1 col-lg-5 col-md-offset-1 col-md-5 col-sm-offset-2 col-sm-8 col-sm-offset-2 col-xs-offset-2 col-xs-8 col-xs-offset-2">
-            <img src="images/photo2annie.png" class="img-responsive">
-          </div>
-          <div class="biographie col-lg-4 col-md-4 col-sm-12 col-xs-12">
-            <h1>Annie Bearez</h1>
-            <p>Quam ob rem id primum videamus, si placet, quatenus amor in amicitia progredi debeat. Numne, si Coriolanus habuit amicos, ferre contra patriam arma illi cum Coriolano debuerunt? num Vecellinum amici regnum adpetentem, num Maelium debuerunt iuvare?
-            Illud tamen clausos vehementer angebat quod captis navigiis, quae frumenta vehebant per flumen, Isauri quidem alimentorum copiis adfluebant, ipsi vero solitarum rerum cibos.</p>
-          </div>
+      <div class="row">
+        <div class="col-lg-offset-1 col-lg-5 col-md-offset-1 col-md-5 col-sm-offset-2 col-sm-8 col-sm-offset-2 col-xs-offset-2 col-xs-8 col-xs-offset-2">
+          <img src="images/photo2annie.png" class="img-responsive">
         </div>
+        <div class="biographie col-lg-4 col-md-4 col-sm-12 col-xs-12">
+          <h1>Annie Bearez</h1>
+          <p>Quam ob rem id primum videamus, si placet, quatenus amor in amicitia progredi debeat. Numne, si Coriolanus habuit amicos, ferre contra patriam arma illi cum Coriolano debuerunt? num Vecellinum amici regnum adpetentem, num Maelium debuerunt iuvare?
+          Illud tamen clausos vehementer angebat quod captis navigiis, quae frumenta vehebant per flumen, Isauri quidem alimentorum copiis adfluebant, ipsi vero solitarum rerum cibos.</p>
+        </div>
+      </div>
     </div>
 
     <div class="index_section3_activites">
@@ -83,7 +91,7 @@
       </div>
     </div>
 
-        <div class="index_section4_blog">
+    <div class="index_section4_blog">
       <div class="row">
         <div class="citation col-lg-offset-1 col-lg-3 col-md-offset-1 col-md-3 col-sm-offset-1 col-sm-10 col-sm-offset-1 col-xs-offset-1 col-xs-10 col-xs-offset-1">"Te justifier ne t'apportera jamais un chameau de plus."</div>
         <div class="blog1 col-lg-offset-1 col-lg-6 col-lg-offset-1 col-md-offset-1 col-md-6 col-md-offset-1 col-sm-offset-1 col-sm-10 col-sm-offset-1 col-xs-offset-1 col-xs-10 col-xs-offset-1">
