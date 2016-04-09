fa3baae8e4ec23465ac5a6c630c354115bf5610d
etidimjav <thomas.javanaud@gmail.com>
2016-04-09 21:23:42 +0200

    Footer fini !

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
 <div class="footer row">
-	<div class="footer_left col-xs-12 col-sm-12 col-md-4 col-lg-4">
+	<div class="footer_left col-xs-12 col-sm-4 col-md-4 col-lg-4">
 		<p>41 Rue Robert Poisson </p>
 		<p>28130 SAINT PIAT </p>
 		<p>PARIS : Place d'Italie, Nationale</p>
 	</div>	
-	<div class="footer_middle col-xs-12 col-sm-12 col-md-4 col-lg-4">
+	<div class="footer_middle col-xs-12 col-sm-4 col-md-4 col-lg-4">
 		<p><img src= "images/telephone.png"> 06 87 68 13 99</p>
 		<p><img src= "images/mail.png"> annie.bearez@wanadoo.fr</p>
 	</div>
-	<div class="footer_right col-xs-12 col-sm-12 col-md-4 col-lg-4">
+	<div class="footer_right col-xs-12 col-sm-4 col-md-4 col-lg-4">
 		<p><a class="liens" href="plandusite.php"> Plan du site </a></p>
-  	<p> <a class="liens" href="mentionslegales.php"> Mentions legales</a> &copy;</p> 
+  		<p> <a class="liens" href="mentionslegales.php"> Mentions legales</a> &copy;</p> 
 		<p>SIRET : 385 293 758 00038</p>
 	</div>
 </div>
