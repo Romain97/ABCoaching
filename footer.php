bb836f7202c3c63d4fc5e72f7a6ba2762e1cda22
etidimjav <thomas.javanaud@gmail.com>
2016-04-11 10:36:29 +0200

    correction footer

--- footer.php
+++ footer.php
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-
-		<script type="text/javascript" src="./js/bootstrap.js"></script>
-		<script type="text/javascript" src="./js/script.js"></script>
-		<link type="text/css" rel="stylesheet" href="./css/bootstrap.css">
-		<link type="text/css" rel="stylesheet" href="./css/footer.css">
-	
-			<div class="row">
-		<div class="footer-main">
-			<div class=" col-xs-3 col-sm-3 col-md-3 col-lg-3 footer-section left">
-				<p>41 Rue Robert Poisson </p>
-				<p>28130 SAINT PIAT </p>
-				<p>PARIS : Place d'Italie, Nationale</p>
-			</div>	
-			<div class="col-xs-4 col-sm-4col-md-4 col-lg-4 footer-section middle">
-				
-					<img src= "images/telephone.png" alt="" title="" class="col-xs-2 col-sm-2 col-md-2 col-lg-2"> 
-					<p class="col-sm-4 col-md-4 col-lg-4">06 87 68 13 99  </p>
-				
-				
-					<img src= "images/mail.png" alt="" title="" class=" col-xs-2 col-sm-2 col-md-2 col-lg-2">
-					<p class="col-sm-4 col-md-4 col-lg-4">annie.bearez@wanadoo.fr</p>
-				
-			</div>
-			<div class=" col-md-4 col-lg-4 footer-section right">
-				<div class="footerdroit">
-				
-				<p> <a class="liens" href="plandusite.php"> Plan du site </a> </p>
-                <p> <a class="liens" href="mentionslegales.php"> Mentions legales</a> &copy;</p> 
-				<p>SIRET : 385 293 758 00038</p>
-				</div>
-			</div>
-		</div>
-			</div>									
-	
-=======
 <div class="footer row">
 	<div class="footer_left col-xs-12 col-sm-4 col-md-4 col-lg-4">
 		<p>41 Rue Robert Poisson </p>
@@ -50,4 +14,3 @@
 		<p>SIRET : 385 293 758 00038</p>
 	</div>
 </div>
->>>>>>> 3a713654f1fd82a7f991ed0855c81a19be12bb85
