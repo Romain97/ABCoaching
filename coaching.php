37b456414b424ef514560fddb79682c1f16cd1e2
romain97 <r.melun@live.fr>
2016-04-12 16:46:44 +0200

    MAJ coaching

--- coaching.php
+++ coaching.php
@@ -9,7 +9,7 @@
 
     <link type="text/css" rel="stylesheet" href="./css/style_coaching.css">
 
-    <title>coaching</title>
+    <title>Coaching</title>
 
   </head>
   
@@ -18,35 +18,35 @@
     <?php
       include("header.php")
     ?>
+
     <div class="row">
-      <div class="col-sm-offset-1 col-sm-6 col-md-6 section2_textebis">
+      <div class="coaching_section1_texte col-lg-offset-1 col-lg-6 col-md-offset-1 col-md-6 col-sm-offset-1 col-sm-6 col-xs-offset-1 col-xs-6">
         <h1>COACHING</h1>
-          <p> Le coaching s'effectue sur une durée de 6 mois, avec 10 séances de 1h à 1h30</p>
-          <h4>Enjeu de l'accompagnement</h4>
-          <ul>
-            <li> franchir un obstacle </li>
-            <li> atteindre un objectif </li>
-            <li> gagner du temps</li>
-          </ul>
-          <h4>Thématiques de l'accompagnement</h4>
-          <ul>
-            <li> intégrer un nouveau poste en entreprise</li>
-            <li> conforter sa place de manager</li>
-            <li> traverser un deuil</li>
-          </ul>
-          <h4>Résultat l'accompagnement</h4>
-          <ul>
-            <li> la conscience de soi </li>
-            <li> l'autonomie </li>
-            <li> la sérénité </li>
-          </ul>          
+        <p> Le coaching s'effectue sur une durée de 6 mois, avec 10 séances de 1h à 1h30</p>
+        <h4>Enjeu de l'accompagnement</h4>
+        <ul>
+          <li>franchir un obstacle</li>
+          <li>atteindre un objectif</li>
+          <li>gagner du temps</li>
+        </ul>
+        <h4>Thématiques de l'accompagnement</h4>
+        <ul>
+          <li>intégrer un nouveau poste en entreprise</li>
+          <li>conforter sa place de manager</li>
+          <li>traverser un deuil</li>
+        </ul>
+        <h4>Résultat l'accompagnement</h4>
+        <ul>
+          <li>la conscience de soi</li>
+          <li>l'autonomie</li>
+          <li>la sérénité</li>
+        </ul>          
       </div>
-      <div class=" col-sm-5 col-md-5 section2_photo">
-        <div>
+      <div class="coaching_section2_photo col-lg-4 col-md-4 hidden-sm hidden-xs">
           <img class="img-coaching" src="./images/coaching_test1.jpg">
-        </div>
       </div>
     </div>
+
     <?php
       include("footer.php")
     ?>
