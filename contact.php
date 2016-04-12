845c65a0893226121680cec21b5254b4d66a617d
FredGOVIGNON <fgovignon@gmail.com>
2016-04-12 21:11:00 +0200

    page contact reparee mais prb marge bottom/trop d'espace

--- contact.php
+++ contact.php
@@ -57,17 +57,16 @@
       </form>
     </div>
 
-    <div class="row section4_maps">
-      <!--Carte de localisation-->
-      <div class="col-xs-12 col-md-11 col-md-offset-1">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2641.7611968077867!2d1.581280550808675!3d48.537808031078235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e41064cb21fb51%3A0x2e86372cadd676!2s41+Rue+Robert+Poisson%2C+28130+Saint-Piat%2C+France!5e0!3m2!1sen!2sus!4v1460479440735" width="600%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>      
-      </div> 
-    </div>
-
-    <div class="section5_sncf">
+    <div class="section4_sncf">
       <img src="./images/train-logo-icon-68833.png">
       <a href="http://www.voyages-sncf.com/billet-train/horaires">Pour consulter les horaires de train</a>
     </div>
+
+    <div class="row section5_maps">
+      <!--Carte de localisation-->
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2641.7611968077867!2d1.581280550808675!3d48.537808031078235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e41064cb21fb51%3A0x2e86372cadd676!2s41+Rue+Robert+Poisson%2C+28130+Saint-Piat%2C+France!5e0!3m2!1sen!2sus!4v1460479440735" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+
 
     <?php
       include("footer.php")
