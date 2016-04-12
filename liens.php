8365c44a87f4200a1b74ba4b61581c673694aa70
FredGOVIGNON <fgovignon@gmail.com>
2016-04-12 19:02:57 +0200

    pages liens et contact mais bug api googlemaps

--- liens.php
+++ liens.php
@@ -24,30 +24,33 @@
 
     <div class="liens_section2">
       <h2 class="titre_page_liens">Je vous invite également à consulter les liens suivants...</h2>
-      <div class="row">
-        <div class="col-lg-10 col-lg-offset-1">
-          <div class="liens_institutionnels col-lg-4 col-md-4 col-sm-12 col-xs-12">
-           <h3 class="liste_liens" align="left"><br><u>Institutionnels</br></u>
+     <div class="row">
+        <div class="liens_instit col-lg-12">
+          
+           <h3 class="liste_liens_instit" align="center">
+          <br><u>Institutionnels</u><br>
            <br><a href="http://www.emccfrance.org/fr/Presentation-401.html">EMCC Association Européenne de coaching, coaching solidaire</a></br>
            <br><a href="http://www.jobetcie.org/">JOB&amp;Cie Association pour le retour à l'emploi</a></br>
            <br><a href="www.ifat.fr">IFAT Institut Français d'Analyse Transactionnelle</a></br>
-           </h3> 
-          </div>
+          </h3>
+        </div>
 
-          <div class="liens_pros col-lg-4 col-md-4 col-sm-12 col-xs-12"> 
-            <h3 class="liste_liens" align="center"><br><u>Professionnels</br></u>
+          <div class="liens_pros col-lg-12 col-md-12 col-sm-12 col-xs-12"> 
+            <h3 class="liste_liens_pros" align="center">
+              <br><u>Professionnels</u></br>
             <br><a href="http://www.sophielemosof.fr/">Sophie Lemosof</a></br>
             </h3>
           </div>
 
-          <div class="liens_presse col-lg-4 col-md-4 col-sm-12 col-xs-12"> 
-            <h3 class="liste_liens" align="right"><br><u>Revue de presse</br></u>
+          <div  class="liens_pros col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h3 class="liste_liens_presse" align="center">
+              <br><u>Revue de presse</br></u>
             <br><a href="http://www.cairn.info/revue-actualites-en-analyse-transactionnelle-2016-1-page-101.htm">Article dans la revue CAIRN sur l'analyse transactionnelle</a></br>
             <br><a href="http://www.lechorepublicain.fr/eure-et-loir/actualite/pays/pays-chartrain/2015/02/23/de-la-communication-non-violente_11339888.html">Article du 23 février 2015 dans l'Echo Républicain</a></br>
-            <br><a href="http://www.lechorepublicain.fr/eure-et-loir/actualite/pays/pays-chartrain/2014/09/19/une-coach-simplante-dans-la-commune_11149875.html">Article du 19 septembre 2014 dans l'Echo Républicain</a></br>      
+            <br><a href="http://www.lechorepublicain.fr/eure-et-loir/actualite/pays/pays-chartrain/2014/09/19/une-coach-simplante-dans-la-commune_11149875.html">Article du 19 septembre 2014 dans l'Echo Républicain</a></br>   
             </h3>
           </div>
-        </div>
+        
       </div>
     </div>
 
