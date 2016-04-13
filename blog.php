9fc916b95876bc00057329d8bea8c5c08d817be9
romain97 <r.melun@live.fr>
2016-04-13 16:50:48 +0200

    MAJ blog

--- blog.php
+++ blog.php
@@ -22,42 +22,79 @@
 
  <div class="container">
     <div class="row">
-      <div class="post col-lg-8 col-md-8">
-        <div id="content">
-          <div class="row">
-            <div class="col-lg-12 col-md-12">
-              <aside>
-              <h2>Article 1</h2>
-                <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
-              </aside>
-            </div>
-            <div class="col-lg-12 col-md-12">
-              <aside>
-                <h2>Article 2</h2>
-                <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
-              </aside>
-            </div>
+      <div class="post col-lg-8 col-md-8 col-lg-offset-1 col-md-offset-1">
+        <div id="content">           
+         <div class="col-lg-12 col-md-12 texte">
+            <h2>Article 1</h2>
+              <p>Le Lorem Ipsum est simplement du faux texte employé dans 
+                la composition et la mise en page avant impression. Le Lorem Ipsum 
+                est le faux texte standard de l'imprimerie depuis les années 1500, 
+                quand un peintre anonyme assembla ensemble des morceaux de texte pour 
+                réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre 
+                cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que 
+                son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce 
+                à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus 
+                récemment, par son inclusion dans des applications 
+                de mise en page de texte, comme Aldus PageMaker.</p>
+          </div>
+          <div class="col-lg-12 col-md-12 texte">
+            <h2>Article 2</h2>
+              <p>Le Lorem Ipsum est simplement du faux texte employé dans 
+                la composition et la mise en page avant impression. Le Lorem Ipsum 
+                est le faux texte standard de l'imprimerie depuis les années 1500, 
+                quand un peintre anonyme assembla ensemble des morceaux de texte pour 
+                réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre 
+                cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que 
+                son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce 
+                à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus 
+                récemment, par son inclusion dans des applications 
+                de mise en page de texte, comme Aldus PageMaker.</p>
+          </div>
+          <div class="col-lg-12 col-md-12 texte">
+              <h2>Article 3</h2>
+              <p>Le Lorem Ipsum est simplement du faux texte employé dans 
+                la composition et la mise en page avant impression. Le Lorem Ipsum 
+                est le faux texte standard de l'imprimerie depuis les années 1500, 
+                quand un peintre anonyme assembla ensemble des morceaux de texte pour 
+                réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre 
+                cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que 
+                son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce 
+                à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus 
+                récemment, par son inclusion dans des applications 
+                de mise en page de texte, comme Aldus PageMaker.</p>
           </div>
         </div>
       </div>
-      <div class="sidebar col-lg-4 col-md-4">
+      <div class="sidebar col-lg-2 col-md-2 col-sm-12 col-xs-12">
         <div class="bloc_sidebar">
           <h3>// ARCHIVES</h3>
           <ul>
             <li class="archive">
               <a href="#">
+                <h5>Avril 2016</h5>
+              </a>
+            </li>
+            <li class="archive">
+              <a href="#">
                 <h5>Mars 2016</h5>
               </a>
             </li>
+            </li>
             <li class="archive">
               <a href="#">
-                <h5>Avril 2016</h5>
+                <h5>Février 2016</h5>
               </a>
             </li>
             </li>
             <li class="archive">
               <a href="#">
-                <h5>Avril 2016</h5>
+                <h5>Janvier 2016</h5>
+              </a>
+            </li>
+            </li>
+            <li class="archive">
+              <a href="#">
+                <h5>Décembre 2015</h5>
               </a>
             </li>
           </ul>
@@ -66,15 +103,8 @@
     </div>
   </div>
 
+  <br></br>
 
-       
-
-       
-        
-
-       
-   
-  
   <?php
     include("footer.php")
   ?>
