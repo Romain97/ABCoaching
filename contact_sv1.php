2a2e6c7307b453a03b7ea3a1065ca0397ba484a9
etidimjav <thomas.javanaud@gmail.com>
2016-04-25 11:31:12 +0200

    maj contact

--- contact_sv1.php
+++ contact_sv1.php
@@ -22,31 +22,16 @@
     <div class="row contact_section1_photo">
     </div>
 
-    <div class="row">
-      
+    <div class="row section2_contact">
       <!--Bloc contact-->
-      <div class="contact_section2 col-lg-offset-1 col-lg-5 col-md-offset-1 col-md-5 col-sm-12 col-xs-12">
-
-      <div class= "contact">
+      <div class="info_contact col-lg-offset-1 col-lg-10 col-lg-offset-1 col-md-offset-1 col-md-10 col-md-offset-1 col-sm-12 col-xs-12">
         <h1><u>Contact</u></h1><br>
-        <p>41 Rue Robert Poisson<br>28130 SAINT PIAT<br>PARIS : Place d'Italie, Nationale<br><br>06 87 68 13 99<br><br>annie.bearez@wanadoo.fr</p><br><br>
+        <p>41 Rue Robert Poisson<br>28130 SAINT PIAT<br>PARIS : Place d'Italie, Nationale<br><br>06 87 68 13 99<br><br>annie.bearez@wanadoo.fr</p>
       </div>
-
-      <div class="sncf">
-        <img src="./images/train-logo-icon-68833.png" alt="Horaires de train">
-        <a href="http://www.voyages-sncf.com/billet-train/horaires">Pour consulter les horaires de train</a>
-      </div>
-      
-      <!--Carte de localisation-->
-      <div class="maps">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2641.7611968077867!2d1.581280550808675!3d48.537808031078235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e41064cb21fb51%3A0x2e86372cadd676!2s41+Rue+Robert+Poisson%2C+28130+Saint-Piat%2C+France!5e0!3m2!1sen!2sus!4v1460479440735" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-      </div>
-
     </div>
-   
-    <div class="section3_formulaire">
+    <div class="row section3_formulaire">
       <!--Formulaire-->
-      <form class="formulaire col-lg-offset-1 col-lg-4 col-lg-offset-1 col-md-offset-1 col-md-4 col-md-offset-1 col-sm-10 col-xs-10">
+      <form class="formulaire col-lg-offset-3 col-lg-6 col-lg-offset-3 col-md-offset-3 col-md-6 col-md-offset-3 col-sm-offset-3 col-sm-6 col-sm-offset-3 col-xs-offset-3 col-xs-6 col-xs-offset-3">
         <h3>Formulaire de contact</h3>
         <div class="form-group">
           <label for="Input_nom"></label>
@@ -71,7 +56,17 @@
         <button type="submit" class="btn btn-primary pull-right">Envoyer</button>
       </form>
     </div>
-      </div>
+
+    <div class="section4_sncf">
+      <img src="./images/train-logo-icon-68833.png" alt="Horaires de train">
+      <a href="http://www.voyages-sncf.com/billet-train/horaires">Pour consulter les horaires de train</a>
+    </div>
+
+    <div class="row section5_maps">
+      <!--Carte de localisation-->
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2641.7611968077867!2d1.581280550808675!3d48.537808031078235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e41064cb21fb51%3A0x2e86372cadd676!2s41+Rue+Robert+Poisson%2C+28130+Saint-Piat%2C+France!5e0!3m2!1sen!2sus!4v1460479440735" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+
 
     <?php
       include("footer.php")
